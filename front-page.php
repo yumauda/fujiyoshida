d9d51689279c74a7853b5cb95bacbc9a79688aa1
yumauda <firststep-yumauda@gmail.com>
2025-12-04 16:18:10 +0900

    最初のコミット

---
+++ front-page.php
@@ -0,0 +1,357 @@
+<?php get_header(); ?>
+<main>
+  <div class="p-top-links">
+    <div class="p-top-links__inner">
+      <ul class="p-top-links__lists">
+        <li class="p-top-links__list">
+          <a href="" class="p-top-links__link">
+            <figure class="p-top-links__img">
+              <picture>
+                <source srcset="<?php echo get_template_directory_uri(); ?>/images/top/top_link1.png" media="(min-width: 768px)" width="1238" height="570" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/top/top_link1_sp.png" alt="" width="355" height="227">
+              </picture>
+            </figure>
+          </a>
+        </li>
+        <li class="p-top-links__list">
+          <a href="" class="p-top-links__link">
+            <figure class="p-top-links__img">
+              <picture>
+                <source srcset="<?php echo get_template_directory_uri(); ?>/images/top/top_link2.png" media="(min-width: 768px)" width="1238" height="570" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/top/top_link2_sp.png" alt="" width="355" height="227">
+              </picture>
+            </figure>
+          </a>
+        </li>
+        <li class="p-top-links__list">
+          <a href="" class="p-top-links__link">
+            <figure class="p-top-links__img">
+              <picture>
+                <source srcset="<?php echo get_template_directory_uri(); ?>/images/top/top_link3.png" media="(min-width: 768px)" width="1238" height="570" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/top/top_link3_sp.png" alt="" width="355" height="227">
+              </picture>
+            </figure>
+          </a>
+        </li>
+        <li class="p-top-links__list">
+          <a href="" class="p-top-links__link">
+            <figure class="p-top-links__img">
+              <picture>
+                <source srcset="<?php echo get_template_directory_uri(); ?>/images/top/top_link4.png" media="(min-width: 768px)" width="1238" height="570" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/top/top_link4_sp.png" alt="" width="355" height="227">
+              </picture>
+            </figure>
+          </a>
+        </li>
+        <li class="p-top-links__list">
+          <a href="" class="p-top-links__link">
+            <figure class="p-top-links__img">
+              <picture>
+                <source srcset="<?php echo get_template_directory_uri(); ?>/images/top/top_link5.png" media="(min-width: 768px)" width="1238" height="570" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/top/top_link5_sp.png" alt="" width="355" height="227">
+              </picture>
+            </figure>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <section class="p-event">
+    <div class="l-inner">
+      <div class="p-event__content">
+        <div class="p-event__left">
+          <div class="p-event__title">
+            <div class="c-section-title">
+              <h2 class="c-section-title__ja">旬なイベント</h2>
+              <p class="c-section-title__en">Seasonal Events</p>
+            </div>
+          </div>
+          <div class="p-event__slider-btn">
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+          </div>
+          <div class="p-event__btn-wrapper">
+            <a href="#" class="c-btn">
+              <p class="c-btn__text">旬なイベント一覧</p>
+              <div class="c-btn__svg">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 30 30">
+                  <defs>
+                    <linearGradient id="linear-gradient" x1="0.193" x2="1" y2="1" gradientUnits="objectBoundingBox">
+                      <stop offset="0" stop-color="#26d0ce" />
+                      <stop offset="1" stop-color="#2c77bc" />
+                    </linearGradient>
+                  </defs>
+                  <g id="グループ_840" data-name="グループ 840" transform="translate(-1830 -991.427)">
+                    <g id="グループ_841" data-name="グループ 841" transform="translate(22 1)">
+                      <circle id="楕円形_22" data-name="楕円形 22" cx="15" cy="15" r="15" transform="translate(1808 990.427)" fill="url(#linear-gradient)" />
+                      <g id="グループ_755" data-name="グループ 755" transform="translate(1817 1002.427)">
+                        <path id="パス_29687" data-name="パス 29687" d="M-11860.351-13710.452h11.07" transform="translate(11860.351 13713.999)" fill="none" stroke="#fff" stroke-width="1" />
+                        <path id="パス_29688" data-name="パス 29688" d="M-11847.689-13715.562l3.082,3.5-3.082,3.5" transform="translate(11856.203 13715.563)" fill="none" stroke="#fff" stroke-width="1" />
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </a>
+          </div>
+
+        </div>
+        <div class="p-event__slider-wrapper">
+          <div class="p-event__slider">
+            <div class="swiper slider2">
+              <!-- Additional required wrapper -->
+              <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                  <a href="#" class="p-event__slider-link">
+                    <figure class="p-event__img">
+                      <img src="<?php echo get_template_directory_uri() ?>/images/top/slider_1.jpg" alt="<?php the_title() ?>" width="370" height="523">
+                    </figure>
+                    <div class="p-event__detail">
+                      <p class="p-event__slider-title">虹の花まつり 2025</p>
+                      <div class="p-event__slider-detail">
+                        <div class="p-event__slider-block">
+                          <p class="p-event__slider-sub">開催期間：</p>
+                          <p class="p-event__slider-text">2025年08月30日(土)〜2025年10月13日(月)</p>
+                        </div>
+                        <div class="p-event__slider-block">
+                          <p class="p-event__slider-sub">開催場所：</p>
+                          <p class="p-event__slider-text">山梨県南都留郡富士河口湖町本栖212<br>富士本栖湖リゾート</p>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div class="swiper-slide">
+                  <a href="#" class="p-event__slider-link">
+                    <figure class="p-event__img">
+                      <img src="<?php echo get_template_directory_uri() ?>/images/top/slider_1.jpg" alt="<?php the_title() ?>" width="370" height="523">
+                    </figure>
+                    <div class="p-event__detail">
+                      <p class="p-event__slider-title">虹の花まつり 2025</p>
+                      <div class="p-event__slider-detail">
+                        <div class="p-event__slider-block">
+                          <p class="p-event__slider-sub">開催期間：</p>
+                          <p class="p-event__slider-text">2025年08月30日(土)〜2025年10月13日(月)</p>
+                        </div>
+                        <div class="p-event__slider-block">
+                          <p class="p-event__slider-sub">開催場所：</p>
+                          <p class="p-event__slider-text">山梨県南都留郡富士河口湖町本栖212<br>富士本栖湖リゾート</p>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div class="swiper-slide">
+                  <a href="#" class="p-event__slider-link">
+                    <figure class="p-event__img">
+                      <img src="<?php echo get_template_directory_uri() ?>/images/top/slider_1.jpg" alt="<?php the_title() ?>" width="370" height="523">
+                    </figure>
+                    <div class="p-event__detail">
+                      <p class="p-event__slider-title">虹の花まつり 2025</p>
+                      <div class="p-event__slider-detail">
+                        <div class="p-event__slider-block">
+                          <p class="p-event__slider-sub">開催期間：</p>
+                          <p class="p-event__slider-text">2025年08月30日(土)〜2025年10月13日(月)</p>
+                        </div>
+                        <div class="p-event__slider-block">
+                          <p class="p-event__slider-sub">開催場所：</p>
+                          <p class="p-event__slider-text">山梨県南都留郡富士河口湖町本栖212<br>富士本栖湖リゾート</p>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div class="swiper-slide">
+                  <a href="#" class="p-event__slider-link">
+                    <figure class="p-event__img">
+                      <img src="<?php echo get_template_directory_uri() ?>/images/top/slider_1.jpg" alt="<?php the_title() ?>" width="370" height="523">
+                    </figure>
+                    <div class="p-event__detail">
+                      <p class="p-event__slider-title">虹の花まつり 2025</p>
+                      <div class="p-event__slider-detail">
+                        <div class="p-event__slider-block">
+                          <p class="p-event__slider-sub">開催期間：</p>
+                          <p class="p-event__slider-text">2025年08月30日(土)〜2025年10月13日(月)</p>
+                        </div>
+                        <div class="p-event__slider-block">
+                          <p class="p-event__slider-sub">開催場所：</p>
+                          <p class="p-event__slider-text">山梨県南都留郡富士河口湖町本栖212<br>富士本栖湖リゾート</p>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                
+              
+              </div>
+              <!-- If we need pagination -->
+              <!--     <div class="swiper-pagination"></div> -->
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  </section>
+  <section class="p-top-information">
+    <div class="l-inner">
+      <div class="p-top-information__content">
+        
+      </div>
+    </div>
+  </section>
+
+  <section class="p-model">
+    <div class="l-inner">
+      <div class="p-model__content">
+        <div class="p-model__title">
+          <div class="c-section-title">
+            <h2 class="c-section-title__ja">おすすめモデルコース</h2>
+            <p class="c-section-title__en">Model Courses</p>
+          </div>
+          <div class="p-model__btn-wrapper">
+            <a href="#" class="c-btn">
+              <p class="c-btn__text">モデルコース一覧</p>
+              <div class="c-btn__svg">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 30 30">
+                  <defs>
+                    <linearGradient id="linear-gradient" x1="0.193" x2="1" y2="1" gradientUnits="objectBoundingBox">
+                      <stop offset="0" stop-color="#26d0ce" />
+                      <stop offset="1" stop-color="#2c77bc" />
+                    </linearGradient>
+                  </defs>
+                  <g id="グループ_840" data-name="グループ 840" transform="translate(-1830 -991.427)">
+                    <g id="グループ_841" data-name="グループ 841" transform="translate(22 1)">
+                      <circle id="楕円形_22" data-name="楕円形 22" cx="15" cy="15" r="15" transform="translate(1808 990.427)" fill="url(#linear-gradient)" />
+                      <g id="グループ_755" data-name="グループ 755" transform="translate(1817 1002.427)">
+                        <path id="パス_29687" data-name="パス 29687" d="M-11860.351-13710.452h11.07" transform="translate(11860.351 13713.999)" fill="none" stroke="#fff" stroke-width="1" />
+                        <path id="パス_29688" data-name="パス 29688" d="M-11847.689-13715.562l3.082,3.5-3.082,3.5" transform="translate(11856.203 13715.563)" fill="none" stroke="#fff" stroke-width="1" />
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </a>
+          </div>
+        </div>
+        <ul class="p-model__grid">
+          <li class="p-model__grid-item">
+            <figure class="p-model__grid-img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/top/recommend_1.png" alt="おすすめグルメコース" width="345" height="280">
+            </figure>
+            <div class="p-model__detail">
+              <p class="p-model__detail-title">おすすめグルメコース</p>
+              <p class="p-model__detail-text">河口湖のご当地グルメを堪能！山梨県名物「ほうとう」をランチに味わう贅沢なグルメコースです。</p>
+            </div>
+          </li>
+          <li class="p-model__grid-item">
+            <figure class="p-model__grid-img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/top/recommend_2.png" alt="周遊バスで河口湖を巡る" width="345" height="280">
+            </figure>
+            <div class="p-model__detail">
+              <p class="p-model__detail-title">周遊バスで河口湖を巡る</p>
+              <p class="p-model__detail-text">富士山や南アルプスなどの山々に囲まれ、自然豊かな観光スポットが山梨には多数あります。そちらをバスで周遊するコースです。</p>
+            </div>
+          </li>
+          <li class="p-model__grid-item">
+            <figure class="p-model__grid-img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/top/recommend_3.png" alt="河口湖でレンタサイクル" width="345" height="280">
+            </figure>
+            <div class="p-model__detail">
+              <p class="p-model__detail-title">河口湖でレンタサイクル</p>
+              <p class="p-model__detail-text">都会を離れて、河口湖周辺でのんびり一人旅。「河口湖周辺でのんびり一人で過ごしたい」という方のためのプランです。</p>
+            </div>
+          </li>
+          <li class="p-model__grid-item">
+            <figure class="p-model__grid-img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/top/recommend_4.png" alt="ファミリーにおすすめコース" width="345" height="280">
+            </figure>
+            <div class="p-model__detail">
+              <p class="p-model__detail-title">ファミリーにおすすめ<br>コース</p>
+              <p class="p-model__detail-text">ファミリーにおすすめ！国の特別名勝昇仙峡・信玄餅工場・桃にぶどう狩りを楽しむモデルコース。</p>
+            </div>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </section>
+  <section class="p-model">
+    <div class="l-inner">
+      <div class="p-model__content">
+        <div class="p-model__title">
+          <div class="c-section-title">
+            <h2 class="c-section-title__ja">特集記事</h2>
+            <p class="c-section-title__en">Special Features</p>
+          </div>
+          <div class="p-model__btn-wrapper">
+            <a href="#" class="c-btn">
+              <p class="c-btn__text">特集記事一覧</p>
+              <div class="c-btn__svg">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 30 30">
+                  <defs>
+                    <linearGradient id="linear-gradient" x1="0.193" x2="1" y2="1" gradientUnits="objectBoundingBox">
+                      <stop offset="0" stop-color="#26d0ce" />
+                      <stop offset="1" stop-color="#2c77bc" />
+                    </linearGradient>
+                  </defs>
+                  <g id="グループ_840" data-name="グループ 840" transform="translate(-1830 -991.427)">
+                    <g id="グループ_841" data-name="グループ 841" transform="translate(22 1)">
+                      <circle id="楕円形_22" data-name="楕円形 22" cx="15" cy="15" r="15" transform="translate(1808 990.427)" fill="url(#linear-gradient)" />
+                      <g id="グループ_755" data-name="グループ 755" transform="translate(1817 1002.427)">
+                        <path id="パス_29687" data-name="パス 29687" d="M-11860.351-13710.452h11.07" transform="translate(11860.351 13713.999)" fill="none" stroke="#fff" stroke-width="1" />
+                        <path id="パス_29688" data-name="パス 29688" d="M-11847.689-13715.562l3.082,3.5-3.082,3.5" transform="translate(11856.203 13715.563)" fill="none" stroke="#fff" stroke-width="1" />
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </a>
+          </div>
+        </div>
+        <ul class="p-model__grid">
+          <li class="p-model__grid-item">
+            <figure class="p-model__grid-img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/top/recommend_5.png" alt="河口湖町で巡る富士絶景" width="345" height="280">
+            </figure>
+            <div class="p-model__detail">
+              <p class="p-model__detail-title">河口湖町で巡る富士絶景</p>
+              <p class="p-model__detail-text">河口湖町周辺には富士山を美しく撮影できるスポットが点在。湖畔や展望台など四季折々の絶景を紹介します。</p>
+            </div>
+          </li>
+          <li class="p-model__grid-item">
+            <figure class="p-model__grid-img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/top/recommend_6.png" alt="河口湖町で彩る紅葉絶景" width="345" height="280">
+            </figure>
+            <div class="p-model__detail">
+              <p class="p-model__detail-title">河口湖町で彩る紅葉絶景</p>
+              <p class="p-model__detail-text">秋の河口湖町は紅葉の名所が点在。湖畔に映える赤や黄の彩りと富士山の共演を楽しめる絶景スポットを紹介します。</p>
+            </div>
+          </li>
+          <li class="p-model__grid-item">
+            <figure class="p-model__grid-img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/top/recommend_7.png" alt="河口湖で巡る癒しの聖地" width="345" height="280">
+            </figure>
+            <div class="p-model__detail">
+              <p class="p-model__detail-title">河口湖で巡る癒しの聖地</p>
+              <p class="p-model__detail-text">河口湖町には自然に囲まれたパワースポットが点在。心身を癒し、富士のエネルギーを感じられる神秘の場所を紹介します。</p>
+            </div>
+          </li>
+          <li class="p-model__grid-item">
+            <figure class="p-model__grid-img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/top/recommend_8.png" alt="河口湖周辺のおすすめキャンプ場" width="345" height="280">
+            </figure>
+            <div class="p-model__detail">
+              <p class="p-model__detail-title">河口湖周辺のおすすめ<br>キャンプ場を紹介</p>
+              <p class="p-model__detail-text">河口湖のキャンプ場は湖畔や林間といった涼しい場所につくられているだけでなく、富士山が見える絶景のロケーションが多いのも魅力です。</p>
+            </div>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </section>
+</main>
+<?php get_footer() ?>
